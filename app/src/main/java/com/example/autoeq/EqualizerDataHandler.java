e4b911aeebc45a4ddb0535588cad17014fd6d0ef
Keny <kmc12436@gmail.com>
2026-06-09 23:11:42 -0300

    New class created

---
+++ app/src/main/java/com/example/autoeq/EqualizerDataHandler.java
@@ -0,0 +1,29 @@
+package com.example.autoeq;
+
+public class EqualizerDataHandler {
+    private GenreEqualizer equalizer;
+
+    public EqualizerDataHandler(GenreEqualizer equalizer) {
+        this.equalizer = equalizer;
+    }
+
+    public GenreEqualizer getEqualizer() {
+        return equalizer;
+    }
+
+    public void setEqualizer(GenreEqualizer equalizer) {
+        this.equalizer = equalizer;
+    }
+
+    private void createEqualizer(GenreEqualizer equalizer) {
+
+    }
+
+    private void saveEqualizer(GenreEqualizer equalizer) {
+
+    }
+
+    private void loadEqualizer(GenreEqualizer equalizer) {
+
+    }
+}
