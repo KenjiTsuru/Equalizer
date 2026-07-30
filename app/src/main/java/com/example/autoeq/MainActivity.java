465afbbd34ff54bb1d2a779c797013d23b49da0a
Keny <kmc12436@gmail.com>
2026-07-30 01:49:07 -0300

    Made it one fragment

--- app/src/main/java/com/example/autoeq/MainActivity.java
+++ app/src/main/java/com/example/autoeq/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
@@ -32,28 +35,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // sets the whole view to activity_main
+        Fragment fragment;
 
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
-        bottomNav.setOnItemSelectedListener(item-> {
-            Fragment fragment;
-            int id = item.getItemId();
+        fragment = new EqualizerEditorFragment();
 
-            if (id == R.id.bottom_nav_home) {
-                fragment = new HomeFragment();
-            } else if (id == R.id.bottom_nav_equalizer) {
-                fragment = new EqualizerEditorFragment();
-            } else {
-                fragment = new SettingsFragment();
-            }
-            getSupportFragmentManager().beginTransaction().replace(R.id.equalizer_fragment_container, fragment)
-                    .commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.equalizer_fragment_container, fragment)
+                .commit();
 
-            return true;
-        });
-
-        if (savedInstanceState == null) {
-            bottomNav.setSelectedItemId(R.id.bottom_nav_home);
-        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -63,5 +51,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
+
+
 
 
